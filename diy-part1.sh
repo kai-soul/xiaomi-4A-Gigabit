da246d52863f0d79f9bb034fd9868bc1b4490bec
kai-soul <81071306+kai-soul@users.noreply.github.com>
2023-05-07 20:13:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,11 +9,15 @@ export shanchu2=$(grep  -a -n -e '&pcie {' target/linux/ramips/dts/mt7621_xiaomi
 export shanchu2=$(expr $shanchu2 - 1)
 export shanchu2=$(echo $shanchu2"d")
 sed -i $shanchu1,$shanchu2 target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
-grep  -Pzo '&spi0[\s\S]*};[\s]*};[\s]*};[\s]*};' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts > youhua.txt
-echo "" >> youhua.txt
-echo "" >> youhua.txt
-export shanchu1=$(expr $shanchu1 - 1)
-export shanchu1=$(echo $shanchu1"r")
+# 在20221001分支里有bug，master正常
+# grep  -Pzo '&spi0[\s\S]*};[\s]*};[\s]*};[\s]*};' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts > youhua.txt
+# echo "" >> youhua.txt
+# echo "" >> youhua.txt
+export jiequ1=$(grep  -a -n -e '&spi0 {' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts|cut -d ":" -f 1)
+export jiequ2=$(grep  -a -n -e '&pcie {' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts|cut -d ":" -f 1)
+export jiequ2=$(expr $jiequ2 - 1)
+export jiequ2=$(echo $jiequ2"p")
+sed -n $jiequ1,$jiequ2 target/linux/ramips/dts/mt7621_youhua_wr1200js.dts >> youhua.txt
 sed -i "$shanchu1 youhua.txt" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
 rm -rf youhua.txt
 ## 2.修改mt7621.mk
